aa81284456bc733e9df92fdfb720f19c63988259
Austin Amento <76490816+puginator@users.noreply.github.com>
2023-10-29 20:36:13 -0700

    initi comiit

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -5,8 +5,6 @@ import {eq} from "drizzle-orm";
 import {headers} from "next/headers";
 import {NextResponse} from "next/server";
 import Stripe from "stripe";
-
-
 export async function POST(req: Request) {
   const body = await req.text();
   const signature = headers().get("Stripe-Signature") as string;
